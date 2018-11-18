d79e63d975b907797cd9f050e007cefa9420b90f
SudoSharma <18308855+SudoSharma@users.noreply.github.com>
2018-11-18 20:32:30 +0000

    [fix] change app to app.pyx

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize([
-        'app.py', 'evaluate.pyx', 'model/bimpm.pyx', 'model/layers.pyx',
+        'app.pyx', 'evaluate.pyx', 'model/bimpm.pyx', 'model/layers.pyx',
         'model/utils.pyx'
     ],
                           annotate=True))
